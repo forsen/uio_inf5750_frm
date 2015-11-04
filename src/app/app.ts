e84b398005fea0f1d8aa37340266bf2129fde5bb
Erik Haider Forsén <erikhf@ifi.uio.no>
2015-11-04 14:41:49 +0100

    Cleaned up unused code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,38 +1,22 @@
 import {bootstrap, Component, CORE_DIRECTIVES} from 'angular2/angular2';
-import {Http, HTTP_PROVIDERS} from 'angular2/http';
 
 @Component({
     selector: 'hello-world',
     directives: [CORE_DIRECTIVES],
-    providers: [HTTP_PROVIDERS],
     template: `
         <h1>Hello World</h1>
         <p>This is just a sample template yeah!</p>
-        <div *ng-if="name">Name from DHIS: {{name}}</div>
-        <p>{{getName()}}</p>
+        <p>{{getApiURI()}}</p>
     `
 })
-class Hello {
-    http: Http;
-    constructor(http: Http){
-        this.http = http;
-    }
 
-    getName(){
+
+class App {
+
+    getApiURI(){
         return dhisAPI;
     }
-/*
-    getAPI(){
-     return this.http.get('../manifest.webapp')
-         .map(res => res.json())
-         .subscribe(
-             data => console.log(data)
-         );
 
-    }
-*/
-
-    public name: string;
 }
 
-bootstrap(Hello);
+bootstrap(App);
