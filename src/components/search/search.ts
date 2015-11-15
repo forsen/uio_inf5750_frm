21341a61f124462c951deb2a1427d82406c68cc6
Erik Haider Forsén <erik@forsen.not>
2015-11-15 14:25:04 +0100

    [search] Moved styles to separate css file

--- src/components/search/search.ts
+++ src/components/search/search.ts
@@ -4,7 +4,8 @@ import {LiveSearch} from "./livesearch";
 @Component({
     selector: 'mou-search',
     directives: [CORE_DIRECTIVES, LiveSearch],
-    templateUrl: './components/search/search.html'
+    templateUrl: './components/search/search.html',
+    styleUrls: ['./components/search/search.css']
 })
 export class Search {
     orgunits: Array<any> = [];
