c1cf798d7e93a98bc1838a900bac80b8b29330e1
Erik Haider Forsén <erikhf@ifi.uio.no>
2015-11-19 19:32:50 +0100

    [sidebar] Showing basic information on side panel

--- src/components/sidebar/sidebar.ts
+++ src/components/sidebar/sidebar.ts
@@ -1,17 +1,27 @@
-import {Component, CORE_DIRECTIVES,} from 'angular2/angular2';
+import {Component, NgFor, NgIf, NgModel, CORE_DIRECTIVES,FORM_DIRECTIVES} from 'angular2/angular2';
 import {Http} from 'angular2/http';
 
 
 @Component({
     selector: 'mou-sidebar',
-    directives: [CORE_DIRECTIVES],
+    directives: [CORE_DIRECTIVES,FORM_DIRECTIVES,NgFor,NgModel,NgIf],
     templateUrl: './components/sidebar/sidebar.html'
 })
 export class Sidebar {
     http: Http;
+    activeOrgUnit: Object;
+    editmode: boolean;
 
     constructor(http:Http){
         this.http = http;
+        editmode = false;
+    }
+
+    update(orgunitId){
+        console.log(orgunitId);
+        this.http.get(dhisAPI + "/api/organisationUnits/" + orgunitId)
+            .map(res => res.json())
+            .subscribe(res => this.activeOrgUnit = res)
     }
 }
 
